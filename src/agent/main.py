778e9938a8cc3ad4c3b8b0e0fcecd97c748b5d76
Andreas Wack <awa@equicon.de>
2026-07-28 12:38:03 +0200

    structure

--- src/agent/main.py
+++
@@ -1,6 +0,0 @@
-"""
-This is the main loop of jarvis miller. He is an ai software developer and is capable of checking a github repo for tickets
-and works on them autonomously. He creates pull requests to be reviewed by his superviser.
-
-Everything is configurated in the config.json (repo, github account data, superviser, etc.)
-"""
